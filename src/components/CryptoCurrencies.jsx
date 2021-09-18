923c8baac26622398a04763955462637856f46ef
Djamal <rust.91@icloud.com>
2021-09-19 01:22:31 +0200

    Layout Building & Sample Data

---
+++ src/components/CryptoCurrencies.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CryptoCurrencies = () => {
+    return (
+        <div>
+            All Crypto Currencies
+        </div>
+    )
+}
+
+export default CryptoCurrencies
---
+++ src/services/crypto-api.js
@@ -0,0 +1,8 @@
+/*var options = {
+	method: 'GET',
+	url: 'https://coinranking1.p.rapidapi.com/exchanges',
+	headers: {
+	  'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+	  'x-rapidapi-key': 'd1f4e9885cmsh49004749392b225p1d7c70jsnef307806feca'
+	}
+};*/
